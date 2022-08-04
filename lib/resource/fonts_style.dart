4625a2aa5bd928d01d687bf947e2d4aef2ae50bb
abdullah_asad <mustafo.nurhayo@gmail.com>
2022-08-04 08:45:58 +0500

    I've added colors and fonts style

---
+++ lib/resource/fonts_style.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_taxi/resource/colors.dart';
+
+class AppFonts {
+  static const headline1 = TextStyle(
+    color: AppColors.blackStyleColor,
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const headline2 = TextStyle(
+    color: AppColors.blackStyleColor,
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const whiteHeadline2 = TextStyle(
+    color: AppColors.primaryWhiteColor,
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const headline3 = TextStyle(
+    color: AppColors.blackStyleColor,
+    fontSize: 13.0,
+    fontWeight: FontWeight.bold,
+  );
+}
