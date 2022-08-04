5eb5ca6b5f5d17a8391d4a0b8c22a7d166704e83
abdullah_asad <mustafo.nurhayo@gmail.com>
2022-08-04 08:46:22 +0500

    I've added colors

---
+++ lib/resource/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // primary white color
+  static const primaryWhiteColor = Color(0xFFFFFFFF);
+
+  // deep green style
+  static const deepGreenStyleColor = Color(0xFF7EAB3A);
+
+  // black style
+  static const blackStyleColor = Color(0xFF3E4958);
+
+  // grey style
+  static const greyStyleColor = Color(0xFFD5DDE0);
+
+  // grey2 style
+  static const grey2StyleColor = Color(0xFF97ADB6);
+
+  // grey3 style
+  static const grey3StyleColor = Color(0xFFF7F8F9);
+
+  // yellow style
+  static const yellowStyleColor = Color(0xFFFFB800);
+
+  //red style
+  static const redStyleColor = Color(0xFFEB001B);
+}
