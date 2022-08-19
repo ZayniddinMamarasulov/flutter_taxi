d2f773161e9db1b9e78815ece8b9c7dd4626e0d6
Shamuratov Oralbay <shamuratovoralbay2003@gmail.com>
2022-08-19 21:06:48 +0500

    easy localization

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_taxi/screens/login_screen.dart';
+import 'package:flutter_taxi/screens/home_screen.dart';
+import 'package:flutter_taxi/screens/register_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -18,20 +19,29 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   startTimer() async {
-    var duration = Duration(seconds: 3);
-    return new Timer(duration, route);
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, route);
   }
 
-  route() {
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => LoginScreen()));
+  Future<bool> getLoginState() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getBool('isLogin') ?? false;
+  }
+
+  route() async {
+    bool isLogin = await getLoginState();
+    isLogin
+        ? Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) =>  HomeScreen()))
+        : Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => RegisterScreen()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
             image: DecorationImage(
                 fit: BoxFit.cover,
                 image: AssetImage('assets/splash_background.png'))),
